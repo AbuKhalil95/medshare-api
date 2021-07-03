901aec93ae7d5e5502d8b24b3b5e496a457de023
Yahya Abu Khalil <yahya_abukhaleel@hotmail.com>
2021-07-03 11:28:52 +0300

    file validation test

--- src/main/resources/static/js/imageUpload.js
+++ src/main/resources/static/js/imageUpload.js
@@ -1,9 +1,12 @@
-const oFile = document.getElementById("imageUpload").files[0];
-
 // TODO: Prevent image upload for profile and item that is more than 1 MB
 // 1 MiB for bytes.
-if (oFile.size > 1048576) {
-  alert("File size must under 1MiB!");
-  return;
-} else {
+function validateSize(input) {
+    const fileSize = input.files[0].size / 1024 / 1024;
+    if (oFile.size > 2) {
+      alert("File size must under 2MiB!");
+      input.value = '';
+      return;
+    } else {
+        
+    }
 }
